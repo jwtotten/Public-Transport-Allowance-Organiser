e0b6b142d5c13d5877070eaf190022cbc8554808
DESKTOP-UK7720Q\jackt <jacktotten@hotmail.co.uk>
2024-03-13 16:30:58 +0000

    Top button on the UI now opens up the fileDialog system and allows the user to select any file. This returns the path to the file.

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
-# This is a sample Python script.
 from pyQT6_UI.landingPage import Ui_Dialog as LandingPageUI
 from PyQt6 import QtWidgets
-from PyQt6.uic import loadUi
-from PyQt6.QtWidgets import QWidget, QStackedWidget, QDialog, QMainWindow
+from PyQt6.QtWidgets import QWidget, QFileDialog, QDialog
 import sys
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
 
 class LandingPage(QDialog, LandingPageUI):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.pushButton.clicked.connect(self.sayHi)
+        self.pushButton.clicked.connect(self.openFileDir)
+        self.pushButton_2.clicked.connect(self.sayHi)
 
 
     def sayHi(self) -> None:
         print('Hi')
+
+    def openFileDir(self) -> str:
+        fileDir = QFileDialog.getOpenFileName(self, "Open file", "", "")
+        print(fileDir[0])
+        return str(fileDir[0])
+
 
 def main() -> None:
     app = QtWidgets.QApplication(sys.argv)
@@ -26,7 +30,5 @@ def main() -> None:
 
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
