95d411e97abcea57d1d601bc0cdc463c1b451e6c
DESKTOP-UK7720Q\jackt <jacktotten@hotmail.co.uk>
2024-03-13 15:29:43 +0000

    Calling first landing page from .ui file -> Todo: make this from the .py file instead

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 # This is a sample Python script.
-
+from pyQT6_UI.landingPage import Ui_Dialog as LandingPageUI
+from PyQt6 import QtWidgets
+from PyQt6.uic import loadUi
+from PyQt6.QtWidgets import QWidget, QStackedWidget, QDialog, QMainWindow
+import sys
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+class LandingPage(QDialog):
+    def __init__(self, parent = None):
+        super().__init__(parent)
+        loadUi('pyQT6_UI/landingPage.ui', self)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    def sayHi(self) -> str:
+        return "Hi"
+
+def main() -> None:
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = LandingPage()
+    window.show()
+    app.exec()
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    main()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
